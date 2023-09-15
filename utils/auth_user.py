f288930c1fe93e2a3c1833d5cbf2fb4b9960c099
Shaban Mohammedsaani Hassan <36283843+shaban00@users.noreply.github.com>
2023-09-14 17:41:58 -0700

    Update auth_user.py

--- utils/auth_user.py
+++ utils/auth_user.py
@@ -1,8 +1,6 @@
-__author__ = 'Shaban Hassan [shaban00]'
-
+__author__ = 'Shaban Mohammedsaani Hassan [shaban00]'
 
 class AuthUser:
-    
     def __init__(self, ref_id, privileges=[]):
         self.ref_id = ref_id
         self.privileges = privileges
