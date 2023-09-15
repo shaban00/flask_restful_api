9e5643560c0319be24cd4e3527d58e5a42dc855b
Shaban Mohammedsaani Hassan <36283843+shaban00@users.noreply.github.com>
2023-09-14 17:44:06 -0700

    Update exceptions.py

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,11 +1,10 @@
-__author__ = 'Shaban Hassan [shaban00]'
+__author__ = 'Shaban Mohammedsaani Hassan [shaban00]'
 
 from typing import Dict
 from flask import make_response, jsonify
 
 
 class BaseError(Exception):
-
     def __init__(self, message: str, error_code: int = 400, payload: [Dict, None] = None, *args) -> None:
         super(BaseError, self).__init__(*args)
         self.message = message
@@ -24,9 +23,7 @@ class BaseError(Exception):
         return response
 
 
-
 class JSONDataValidatorError(BaseError):
-
     def __init__(self, message: str, payload: [Dict, None], error_code: int = 422) -> None:
         super(JSONDataValidatorError, self).__init__(
             message=message, error_code=error_code, payload=payload
@@ -34,7 +31,6 @@ class JSONDataValidatorError(BaseError):
 
 
 class AuthenticationError(BaseError):
-
     def __init__(self):
         super(AuthenticationError, self).__init__(
             message="Authentication Failed", error_code=401
@@ -42,7 +38,6 @@ class AuthenticationError(BaseError):
 
 
 class PrivilegesError(BaseError):
-
     def __init__(self):
         super(PrivilegesError, self).__init__(
             message="Privileges Error, Access Denied", error_code=401
@@ -50,7 +45,6 @@ class PrivilegesError(BaseError):
 
 
 class InvalidAuthorizationHeader(BaseError):
-
     def __init__(self):
         super(InvalidAuthorizationHeader, self).__init__(
             message="Invalid Authorization Header", error_code=401
@@ -58,7 +52,6 @@ class InvalidAuthorizationHeader(BaseError):
 
 
 class IntegrityError(BaseError):
-    
     def __init__(self, message: str):
         super(IntegrityError, self).__init__(
             message=message, error_code=400
@@ -66,7 +59,6 @@ class IntegrityError(BaseError):
 
 
 class InvalidArgumentError(BaseError):
-
     def __init__(self, message: str):
         super(InvalidArgumentError, self).__init__(
             message=message, error_code=400
